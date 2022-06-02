dc951394c62019d208d3ccedfcc151a8195501cf
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-06-02 17:42:52 +0000

    script 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - return size of data types
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %ld byte(S)\n", sizeof(a));
+printf("Size of an int: %ld byte(s)\n", sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+printf("Size of a float: %ld byte(s)\n", sizeof(e));
+
+return (0);
+}
