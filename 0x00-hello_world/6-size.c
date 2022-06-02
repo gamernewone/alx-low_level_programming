28c3543a99e6a70a094bb46ddd77c5a7a891591e
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-06-02 17:49:43 +0000

    script 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,11 @@
 
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-printf("Size of a char: %ld byte(S)\n", sizeof(a));
-printf("Size of an int: %ld byte(s)\n", sizeof(b));
-printf("Size of a long int: %ld byte(s)\n", sizeof(c));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
-printf("Size of a float: %ld byte(s)\n", sizeof(e));
+printf("Size of a char: %ld byte(S)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
 return (0);
 }
