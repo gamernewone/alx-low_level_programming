815cf43e44e5bb90a289f8c0cb0c71451b673296
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-06-02 17:14:10 +0000

    solution 4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-int main(void) 
+/**
+ * main - print a string
+ *
+ * Return: return 0
+ */
+int main(void)
 {
     /* my first program in C */
-    char str1[52];
+char str1[52];
 
-    strcpy(str1, "\"Programming is like building a multilingual puzzle");
-   
-    puts(str1);
-   
-    return (0);
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+puts(str1);
+
+return (0);
 }
