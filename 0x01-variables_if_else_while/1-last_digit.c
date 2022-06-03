4bc1db2659940b9121066c668b264b9628f7ea6f
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-06-03 14:17:47 +0000

    fixes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ last_digit = n % 10;
 
 if (last_digit > 5)
 {
-printf("Last digit of %d is %ld and is greater than 5\n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit < 6 && last_digit != 0)
 {
@@ -28,7 +28,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 else
 {
-printf("Last digit of %d is %ld and is 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 return (0);
 }
