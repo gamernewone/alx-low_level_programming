2f14ffe1499929e74d0381e85b13afaedda3e104
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-06-15 17:22:50 +0000

    fixes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints input string in reverse
+ * @s: input string
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+int count = 0;
+
+while (*(s + count) != '\0')
+{
+s++;
+count++;
+}
+while (count > 0)
+{
+s--;
+_putchar(*s);
+count--;
+}
+_putchar('\n');
+
+}
