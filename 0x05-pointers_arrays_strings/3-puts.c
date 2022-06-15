dd2e0dab58d2a7dbed1812922909e8c9dd37d022
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-06-15 14:24:14 +0000

    fixes

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "_putchar"
+/**
+ * _puts - Print a char
+ * @str: the string to write
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+
+char character = *str;
+for ( ; character != '\0'; character = *++str)
+{
+
+_putchar(character);
+
+}
+
+}
