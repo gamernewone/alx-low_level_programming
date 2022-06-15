6730041ba6db19c05817c67b4f403f7eeb1c0e86
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-06-15 02:36:59 +0000

    trials

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+
+/*
+ * void - none
+ *
+ * retuns - nothing
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+*a = c;
+*b = *a;
+*a = c;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,9 @@
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; *s  <= *(s + 18); i++)
+{
+}
+return (i);
+}
