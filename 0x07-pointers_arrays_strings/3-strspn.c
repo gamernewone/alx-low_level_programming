56432c24b2b83d1437586151d7e6198c24b3b9bb
Rocket_1 <37631123+gamernewone@users.noreply.github.com>
2022-07-06 00:00:56 +0000

    magic 2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,32 @@
 #include "main.h"
 
+/**
+ *_strspn - count the number of string in s that are also in accpt "
+ *@s: content is to be processed
+ *@accept: Source of data to check
+ *Return: count
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
-    int k, t;
-    unsigned int count = 0, dre = 0;
+int k, t;
+unsigned int count = 0, dre = 0;
 
-    for (k = 0; *(s + k) != '\0' && (*(s + k)) != 32; k++)
-    {
-        for (t = 0; (*(accept + t)) != '\0'; t++)
-        {
-            if ((*(s + k)) == (*(accept + t)))
-                count++;
+for (k = 0; *(s + k) != '\0' && (*(s + k)) != 32; k++)
+{
+for (t = 0; (*(accept + t)) != '\0'; t++)
+{
+if ((*(s + k)) == (*(accept + t)))
+{
+count++;
+}
+}
+dre++;
+if (dre != count)
+{
+break;
 
-        }
-        dre++;
-        if (dre != count)
-            break;
+}
 
-    }
-
-    return(count);
-
+return (count);
 }
