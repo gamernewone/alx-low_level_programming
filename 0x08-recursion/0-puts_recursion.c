1c11fa79471f6d2f49760b0ea1c5feaaece52468
gamernewone <37631123+gamernewone@users.noreply.github.com>
2022-07-10 17:30:22 +0000

    added put recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_puts_recursion - print a string using recursion
+* @s: string to print
+*Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+_putchar (*s);
+_puts_recursion(s + 1);
